8d9bbb06afdbdb52385a777af106cb7c82a88c0e
Aryan Tayal <bookworms@ariansh.com>
2023-05-06 21:02:34 +0530

    prefix expense routes properly

--- server/routes/expense.js
+++ server/routes/expense.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const Expense = require("../models/expense");
 const Category = require("../models/category");
